f8090c942379b3697776c13c6efc2e2f0fca4c8f
marouenj <marouen.jilani@gmail.com>
2016-04-21 11:43:36 +0900

    [loader] move to appropriate location

--- agent/load.go
+++ agent/load.go
@@ -18,6 +18,23 @@ type ChannelGroup struct {
 
 type ChannelGroups []ChannelGroup
 
+// load a json file into a ChannelGroups
+func unmarshal(prefix string, name string) (ChannelGroups, error) {
+	in := filepath.Join(prefix, name)
+	file, err := ioutil.ReadFile(in)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading '%s': %s", in, err)
+	}
+
+	var channelGroups ChannelGroups
+	err = json.Unmarshal(file, &channelGroups)
+	if err != nil {
+		return nil, fmt.Errorf("Error decoding '%s': %s", in, err)
+	}
+
+	return channelGroups, nil
+}
+
 type Loader struct {
 	Urls []string
 }
@@ -92,22 +109,6 @@ func (l *Loader) Load(file string) error {
 	sort.Strings(l.Urls)
 
 	return nil
-}
-
-func unmarshal(prefix string, name string) (ChannelGroups, error) {
-	in := filepath.Join(prefix, name)
-	file, err := ioutil.ReadFile(in)
-	if err != nil {
-		return nil, fmt.Errorf("Error reading '%s': %s", in, err)
-	}
-
-	var channelGroups ChannelGroups
-	err = json.Unmarshal(file, &channelGroups)
-	if err != nil {
-		return nil, fmt.Errorf("Error decoding '%s': %s", in, err)
-	}
-
-	return channelGroups, nil
 }
 
 func forEachFile(prefix string, name string) (*list.List, error) {
