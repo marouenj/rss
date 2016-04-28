2d7042881321bdac0fd3781c20af5e1e1ce6d0e3
marouenj <marouen.jilani@gmail.com>
2016-04-28 17:51:54 +0900

    output to project base dir

--- docker/install.sh
+++ docker/install.sh
@@ -6,6 +6,6 @@ docker run \
        -v $(pwd)/rss.go:/go/src/github.com/marouenj/rss/rss.go \
        -v $(pwd)/agent:/go/src/github.com/marouenj/rss/agent \
        -v $(pwd)/util:/go/src/github.com/marouenj/rss/util \
-       -v /tmp:/go/bin \
+       -v $(pwd):/go/bin \
 golang:1.6 \
 go install github.com/marouenj/rss
