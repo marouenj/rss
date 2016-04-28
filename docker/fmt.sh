5e72a32e6b4b328c52ea5a3dea180ccdafa39b50
marouenj <marouen.jilani@gmail.com>
2016-04-28 16:41:45 +0900

    [rss] format main file

--- docker/fmt.sh
+++ docker/fmt.sh
@@ -3,6 +3,7 @@
 docker run \
        --rm \
        --name go \
+       -v $(pwd)/rss.go:/go/src/github.com/marouenj/rss/rss.go \
        -v $(pwd)/agent:/go/src/github.com/marouenj/rss/agent \
        -v $(pwd)/util:/go/src/github.com/marouenj/rss/util \
 golang:1.6 \
